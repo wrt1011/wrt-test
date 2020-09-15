594c169e70ab7d08215cba5ba82bceb4da73d193
wrt1011 <1097105779@qq.com>
2020-09-15 12:46:21 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
-form django.http import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     return HttpResponse('index')
+def login(request):
+    return redirect('/index')
